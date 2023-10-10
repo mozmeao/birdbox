5834d29d4c1b647620561af0c51ea0a7c7e7beac
Steve Jalim <stevejalim@mozilla.com>
2023-10-10 11:47:20 +0100

    Add factories and pytest fixture for bootstrapping a simple blog

--- birdbox/microsite/tests/factories.py
+++ birdbox/microsite/tests/factories.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+import factory
 import wagtail_factories
 
 from microsite import models
@@ -9,6 +10,25 @@ from microsite import models
 
 class HomePageFactory(wagtail_factories.PageFactory):
     title = "Test Home Page"
+    live = True
+    slug = "homepage"
 
     class Meta:
         model = models.HomePage
+
+
+class BlogIndexPageFactory(wagtail_factories.PageFactory):
+    title = "Blog Index"
+    live = True
+
+    class Meta:
+        model = models.BlogIndexPage
+
+
+class BlogPageFactory(wagtail_factories.PageFactory):
+    feed_image = factory.SubFactory(
+        wagtail_factories.ImageChooserBlockFactory,
+    )
+
+    class Meta:
+        model = models.BlogPage
--- conftest.py
+++ conftest.py
@@ -2,10 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+from datetime import date
+
 import pytest
 import wagtail_factories
 
-from microsite.tests.factories import HomePageFactory
+from microsite.models import HomePage
+from microsite.tests.factories import BlogIndexPageFactory, BlogPageFactory, HomePageFactory
 
 
 @pytest.fixture
@@ -19,4 +22,35 @@ def bootstrap_minimal_site(
     return wagtail_factories.SiteFactory(
         root_page=homepage,
         hostname=client._base_environ()["SERVER_NAME"],
+    )
+
+
+@pytest.fixture
+def homepage(bootstrap_minimal_site):
+    return HomePage.objects.get()
+
+
+@pytest.fixture
+def minimal_site_with_blog(bootstrap_minimal_site):
+    homepage = HomePage.objects.get()
+
+    blog_index = BlogIndexPageFactory(
+        parent=homepage,
+    )
+
+    BlogPageFactory(
+        parent=blog_index,
+        title="blog post 1",
+        date=date(2023, 5, 1),
+    )
+    BlogPageFactory(
+        parent=blog_index,
+        title="blog post 2 (featured)",
+        date=date(2023, 5, 11),
+        is_featured=True,
+    )
+    BlogPageFactory(
+        parent=blog_index,
+        title="blog post 3",
+        date=date(2023, 6, 12),
     )
