c87a7fc273eb2e324d65fec5a21fa20c2ac54391
Steve Jalim <stevejalim@mozilla.com>
2023-10-19 15:07:27 +0100

    Refactor to make things simpler and more robust, following CR

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -4,18 +4,16 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-// Needs dnt-helper.js to be executed first and window.Mozilla.googleTagId to be set
+var dntEnabled = require("@mozmeao/dnt-helper");
 
 (function () {
     "use strict";
-
-    // dnt-helper.js sets the window.Mozilla namespace
-    var GOOGLE_TAG_ID = window.Mozilla.googleTagId;
+    const googleTagId = document.documentElement.dataset.gtmId;
 
     // If doNotTrack is not enabled, it is ok to add GTM
     // @see https://bugzilla.mozilla.org/show_bug.cgi?id=1217896 for more details
     // prettier-ignore
-    if (typeof Mozilla.dntEnabled === 'function' && !Mozilla.dntEnabled() && GOOGLE_TAG_ID) {
+    if (typeof dntEnabled === 'function' && !dntEnabled() && googleTagId) {
 
         window.dataLayer = window.dataLayer || [];
 
@@ -23,13 +21,11 @@
             w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});f=d.getElementsByTagName(s)[0];
             k=i.length;q='//www.googletagmanager.com/gtm.js?id=@&l='+(l||'dataLayer');
             while(k--){j=d.createElement(s);j.async=!0;j.src=q.replace('@',i[k]);f.parentNode.insertBefore(j,f);}
-        }(window,document,'script','dataLayer',[GOOGLE_TAG_ID]));
+        }(window,document,'script','dataLayer',[googleTagId]));
 
         // Google tag (gtag.js)
-        if (!window.Mozilla.dntEnabled()) {
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '{{GOOGLE_TAG_ID}}');
-        }
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '{{googleTagId}}');
     }
 })();
