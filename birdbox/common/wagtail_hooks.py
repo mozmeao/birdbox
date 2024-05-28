d40f690e2c191d7d34e62eb5b9e8db81e0c7fb7b
Steve Jalim <stevejalim@mozilla.com>
2024-05-28 15:22:35 +0100

    Refactor how we use JS to update the ThemedColorBlock select's option labels
    Instead of an eventListener, which has limited reach, we instead use
    Wagtail's "telepath" library's way of doing things
    
    See https://docs.wagtail.org/en/stable/advanced_topics/customisation/streamfield_blocks.html#additional-javascript-on-structblock-forms

--- birdbox/common/wagtail_hooks.py
+++ birdbox/common/wagtail_hooks.py
@@ -2,22 +2,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from django.templatetags.static import static
-from django.utils.html import format_html_join
 from django.utils.safestring import mark_safe
 
 from wagtail import hooks
 
 
 @hooks.register("insert_editor_js")
-def patch_in_dynamic_theme_color_names():
+def patch_in_dynamic_theme_color_name():
     # Each theme has a set of custom/specific colors associated with it,
     # sharing the same CSS classnames, but varying based on theme.
-    # microsite.blocks.ColorThemBlock has values which are the CSS classes to use
+    # common.blocks.ThemedColorBlock has values which are the CSS classes to use
     # and, by default, the _label_ for each option is, basically, the CSS class
-    # name. To make the editor experience better, we swap in theme-specific
-    # human-friendly labels when the editing UI is rendered, based on the
-    # theme selected for the birdbox site
+    # name.
+    # To make the editor experience better, we swap in theme-specific
+    # human-friendly labels when the editing UI is rendered (see
+    # common.blocks.ThemedColorBlockAdapter) based on the
+    # theme selected for the birdbox site. This hook ensure the currently
+    # selected theme is available as a data-attr on the `body` node
 
     from microsite.models import MicrositeSettings
 
@@ -27,18 +28,9 @@ def patch_in_dynamic_theme_color_names():
     except AttributeError:
         theme_name = "mozilla"
 
-    js_files = [
-        "js/color-theme-block-labels.js",
-    ]
-    js_includes = format_html_join("\n", '<script src="{0}"></script>', ((static(filename),) for filename in js_files))
-
-    return js_includes + mark_safe(
+    return mark_safe(
         """
-        <script>
-            window.addEventListener("DOMContentLoaded", () => {
-                window.Birdbox.themeColorLabelFixup("%s");
-            });
-        </script>
+        <div id="bb-site-theme" data-site-theme="%s"></div>
         """
         % theme_name  # noqa: F522 F524  # Old-style formatting needed to avoid breaking hook rendering
     )
